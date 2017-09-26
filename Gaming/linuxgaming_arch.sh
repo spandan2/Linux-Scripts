6d36290985989732d316a3ac135bbc140100b74b
spandan2 <spandanghosh2@gmail.com>
2017-09-26 15:56:06 +0530

    Version 1.0

---
+++ Gaming/linuxgaming_arch.sh
@@ -0,0 +1,17 @@
+echo "Hi.. Please ensure you have a working internet connection....";sleep 2;
+echo "This is Spandan Ghosh and you are about to start gaming in linux... Please enter your password when prompted..";sleep 2;
+pacman -S wine;
+wget http://winetricks.org/winetricks;
+sudo chmod +x winetricks;
+sh winetricks directx9;
+sh winetricks vcrun6sp6;
+sleep 5;
+clear;
+echo "You are now ready to install a few Games";
+echo "If you are installing from an image like a .iso file, goto"
+echo "Just browse to the directory containing the setup file..  ";
+echo "Right click and Select 'Open Terminal Here'..";
+echo "And Just run the setup file using the wine prefix...";
+echo "Eg:-  wine setup.exe";
+echo "Congratulations!! You have run the best linux script there ever was..";
+echo "Now shut this Goddamn terminal down and enjoy...";
